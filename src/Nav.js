ced6a3fc6505ab903ea4faeed352d16b0849c314
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-24 12:22:54 -0300

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,36 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
+
 import { NavLink } from "react-router-dom";
 
 export default function Nav() {
   return (
     <div className="Navbar">
-      <Navbar>
-        <NavLink className="nav-item nav-link" to="/">
-          Curriculum
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/downloads">
-          Downloads
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/journals">
-          Journals
-        </NavLink>
-      </Navbar>
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="navbar-nav">
+            <NavLink className="nav-item nav-link" to="/">
+              Curriculum
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/downloads">
+              Downloads
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/journals">
+              Journals
+            </NavLink>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
