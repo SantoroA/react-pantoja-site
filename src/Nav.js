399f795a0f4720f8e96ec2176efdc7ac7834e136
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-24 12:09:49 -0300

    navbar debugging

--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,21 @@
 import React from "react";
-// import Navbar from "react-bootstrap/Navbar";
+import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
 
 export default function Nav() {
   return (
     <div className="Navbar">
-      <NavLink className="nav-item nav-link" to="/">
-        Curriculum
-      </NavLink>
-      <NavLink className="nav-item nav-link" to="/downloads">
-        Downloads
-      </NavLink>
-      <NavLink className="nav-item nav-link" to="/journals">
-        Journals
-      </NavLink>
+      <Navbar>
+        <NavLink className="nav-item nav-link" to="/">
+          Curriculum
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/downloads">
+          Downloads
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/journals">
+          Journals
+        </NavLink>
+      </Navbar>
     </div>
   );
 }
