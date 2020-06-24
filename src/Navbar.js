e14f6e534b0daba3e209531331c3b358b324f910
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-24 11:49:56 -0300

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,9 +5,17 @@ export default function Navbar() {
   return (
     <div className="Navbar">
       <nav class="navbar navbar-dark bg-dark">
-        <NavLink to="/">Curriculum</NavLink>
-        <NavLink to="/downloads">Downloads</NavLink>
-        <NavLink to="/journals">Journals</NavLink>
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/">
+            Curriculum
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/downloads">
+            Downloads
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/journals">
+            Journals
+          </NavLink>
+        </div>
       </nav>
     </div>
   );
