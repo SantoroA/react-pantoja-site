206c82d2e557c2fe6e40c318f9110255f599bfdd
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-24 11:47:17 -0300

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,9 +4,11 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="Navbar">
-      <NavLink to="/">Curriculum</NavLink>
-      <NavLink to="/downloads">Downloads</NavLink>
-      <NavLink to="/journals">Journals</NavLink>
+      <nav class="navbar navbar-dark bg-dark">
+        <NavLink to="/">Curriculum</NavLink>
+        <NavLink to="/downloads">Downloads</NavLink>
+        <NavLink to="/journals">Journals</NavLink>
+      </nav>
     </div>
   );
 }
