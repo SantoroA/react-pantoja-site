3e6f3b9709734f5406e74867f5521ef55f3b56b7
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-24 12:52:00 -0300

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -57,7 +57,7 @@ export default function Nav() {
               </div>
             </div>
 
-            {/* parceiros */}
+            {/* colaboradores */}
 
             <div className="nav-item dropdown">
               <a
@@ -84,6 +84,40 @@ export default function Nav() {
                 </NavLink>
               </div>
             </div>
+
+            {/* cursos */}
+            <div className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Cursos
+              </a>
+              <div
+                className="dropdown-menu"
+                id="dropdown-menu"
+                aria-labelledby="navbarDropdown"
+              >
+                <NavLink className="nav-item nav-link" to="/patologia">
+                  Patologia
+                </NavLink>
+                <NavLink
+                  className="nav-item nav-link"
+                  to="/sistemas-estruturais"
+                >
+                  Sistemas Estruturais
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/reforco-estrutural">
+                  Reforço Estrutrual
+                </NavLink>
+              </div>
+            </div>
+            {/* fim cursos  */}
           </div>
         </div>
       </nav>
